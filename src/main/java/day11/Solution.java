c4def4395c0d5fd4f94d1fafcaba88406aea1e46
Gjorgji Kirkov <gjorgjikirkov@gmail.com>
2020-12-12 21:28:04 +0100

    barebones setup for day11

---
+++ src/main/java/day11/Solution.java
@@ -0,0 +1,15 @@
+package day11;
+
+import utils.PathUtil;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.util.List;
+
+public class Solution {
+  public static void main(String[] args) throws URISyntaxException, IOException {
+    List<String> lines = Files.readAllLines(PathUtil.getFilePath("day11/puzzle-input.txt"));
+    // todo: implement solutions
+  }
+}
